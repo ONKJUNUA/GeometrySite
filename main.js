e2859f09a370f61908e4db0afaa273e3266ce02f
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-02-27 21:41:14 +0100

    adding light helper and grid helper

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import * as THREE from 'https://threejs.org/build/three.module.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -18,12 +18,19 @@ pointLight.position.set(0,0,5)
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight)
 
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper)
+
+const gridHelper = new THREE.GridHelper(200,50)
+scene.add(gridHelper)
+
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
     torus.rotation.y += 0.01;
     torus.rotation.x -= 0.1;
+
 	renderer.render( scene, camera );
 }
 animate();
